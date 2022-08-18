659deb936dfc4b81a85800fcc4af1bc922e4782f
Tom Arild Jakobsen <tajakobsen@gmail.com>
2022-08-18 09:47:00 +0200

    Update dependencies and remove unused dependencies

--- src/main/resources/types.ts
+++ src/main/resources/types.ts
@@ -1,21 +1,19 @@
-declare const __non_webpack_require__: (path: string) => any;
-
-declare const resolve: (path: string) => any;
-
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare const app: {
-  name: string,
-  version: string,
-  config: { [key: string]: string }
-}
+  name: "no.item.cristin";
+  version: string;
+  config: { [key: string]: string };
+};
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare const log: {
-  info: (...args: any[]) => void,
-  warning: (...args: any[]) => void,
-  error: (...args: any[]) => void
-}
+  info: (...args: any[]) => void;
+  warning: (...args: any[]) => void;
+  error: (...args: any[]) => void;
+};
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare const __: {
-  newBean: (bean: string) => any,
-  toNativeObject: (beanResult: any) => any
-}
+  newBean: (bean: string) => any;
+  toNativeObject: (beanResult: any) => any;
+};
