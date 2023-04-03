645c1d935b8a3cd2ca13ef7c8eb6d768f8b909ce
Marcus Scheie <marcus.scheie@item.no>
2023-04-03 09:07:10 +0200

    Edit result-contributors config

--- src/main/resources/main.ts
+++ src/main/resources/main.ts
@@ -65,7 +65,7 @@ runAsSu(() => {
     },
     {
       name: "import-result-contributors",
-      enabled: !notNullOrUndefined(app.config.contributors),
+      enabled: app.config.importResultContributors !== "disabled",
       repo: REPO_CRISTIN_RESULT_CONTRIBUTORS,
       cron: "30 2 * * *", // 02:30
     },
